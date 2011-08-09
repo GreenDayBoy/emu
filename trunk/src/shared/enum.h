6c0120cd45aa7bb3beba7237d878afd00b17840a
bezloginu666@gmail.com <bezloginu666@gmail.com@5adc7dac-3e50-ac3d-f90e-e331e26be68f>
2011-08-09 17:58:28 +0000

    many changes [;.

--- trunk/src/shared/enum.h
+++ trunk/src/shared/enum.h
@@ -5,41 +5,41 @@
 #include <vector>
 
 namespace eMUShared {
-	namespace dataServerProtocol_e {
-		enum type_t {
-			_queryException = 0x00,
-			_accountManage = 0xF1,
-			_characterManage = 0xF3,
-		};
+namespace dataServerProtocol_e {
+enum type_t {
+    _queryException = 0x00,
+    _accountManage = 0xF1,
+    _characterManage = 0xF3,
+};
 
-		namespace accountManage_e {
-			enum type_t {
-				_check = 0x01,
-				_logout = 0x02
-			};
-		};
+namespace accountManage_e {
+enum type_t {
+    _check = 0x01,
+    _logout = 0x02
+};
+};
 
-		namespace characterManage_e {
-			enum type_t {
-				_list = 0x00,
-				_create = 0x01,
-				_delete = 0x02,
-				_select = 0x03,
-				_save = 0x04
-			};
-		};
-	};
+namespace characterManage_e {
+enum type_t {
+    _list = 0x00,
+    _create = 0x01,
+    _delete = 0x02,
+    _select = 0x03,
+    _save = 0x04
+};
+};
+};
 
-	namespace characterRace_e {
-		enum type_t {
-			_darkWizard = 0,
-			_darkKnight = 16,
-			_elf = 32,
-			_magicGladiator = 48,
-			_darkLord = 64,
-			_none = 0xFF
-		};
-	};
+namespace characterRace_e {
+enum type_t {
+    _darkWizard = 0,
+    _darkKnight = 16,
+    _elf = 32,
+    _magicGladiator = 48,
+    _darkLord = 64,
+    _none = 0xFF
+};
+};
 };
 
 #endif // eMU_SHARED_ENUM_H
