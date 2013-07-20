6f34b8c935b97d731ae0e7b342443272f04c1243
bezloginu666@gmail.com <bezloginu666@gmail.com@5adc7dac-3e50-ac3d-f90e-e331e26be68f>
2013-07-20 18:33:37 +0000

    Added xml reader.

---
+++ trunk/src/core/common/xmlReader.cpp
@@ -0,0 +1,44 @@
+#include <core/common/xmlReader.hpp>
+#include <core/common/exception.hpp>
+
+namespace eMU {
+namespace core {
+namespace common {
+
+XmlReader::XmlReader():
+  currentNode_(nullptr) {}
+
+void XmlReader::parse(std::string content, std::string firstNodeName) {
+    if(content.empty()) {
+        Exception exception; exception.in() << "Got empty content!";
+        throw exception;
+    }
+
+    document_.parse<0>(const_cast<char*>(content.c_str()));
+    currentNode_ = document_.first_node(const_cast<char*>(firstNodeName.c_str()));
+
+    if(this->end()) {
+        Exception exception; exception.in() << "First node name does not match! name: " << firstNodeName;
+        throw exception;
+    }
+
+    currentNode_ = currentNode_->first_node();
+}
+
+bool XmlReader::end() {
+    return currentNode_ == nullptr;
+}
+
+void XmlReader::next() {
+    if(!this->end()) {
+        currentNode_ = currentNode_->next_sibling();
+    }
+}
+
+void XmlReader::clear() {
+    document_.clear();
+}
+
+}
+}
+}
---
+++ trunk/tst/ut/tests/core/common/xmlReaderTest.cpp
@@ -0,0 +1,95 @@
+#include <gtest/gtest.h>
+
+#include <core/common/xmlReader.hpp>
+#include <core/common/exception.hpp>
+
+class XmlReaderTest: public ::testing::Test {
+protected:
+    void prepareContent() {
+        content_ = "<servers> \
+                        <server code=\"21\" name=\"eMU_Test\" address=\"localhost\" port=\"55901\" /> \
+                        <server code=\"22\" name=\"eMU_Test2\" address=\"127.0.0.1\" port=\"55902\" /> \
+                   </servers>";
+    }
+
+    void checkRecord(uint16_t code, const std::string &name, const std::string &address, uint16_t port) {
+        EXPECT_EQ(code, xmlReader_.get<uint16_t>("server", "code"));
+        EXPECT_EQ(name, xmlReader_.get<std::string>("server", "name"));
+        EXPECT_EQ(address, xmlReader_.get<std::string>("server", "address"));
+        EXPECT_EQ(port, xmlReader_.get<uint16_t>("server", "port"));
+    }
+
+    eMU::core::common::XmlReader xmlReader_;
+    std::string content_;
+};
+
+TEST_F(XmlReaderTest, parseCorrectContent) {
+    prepareContent();
+
+    xmlReader_.parse(content_, "servers");
+    ASSERT_FALSE(xmlReader_.end());
+    checkRecord(21, "eMU_Test", "localhost", 55901);
+
+    xmlReader_.next();
+    ASSERT_FALSE(xmlReader_.end());
+    checkRecord(22, "eMU_Test2", "127.0.0.1", 55902);
+
+    xmlReader_.next();
+    ASSERT_TRUE(xmlReader_.end());
+
+    xmlReader_.next();
+    ASSERT_TRUE(xmlReader_.end());
+
+    xmlReader_.clear();
+}
+
+TEST_F(XmlReaderTest, exceptionShouldBeThrownWhenContentIsEmpty) {
+    bool exceptionThrown = false;
+
+    try {
+        xmlReader_.parse(content_, "servers");
+    } catch(eMU::core::common::Exception &exception) {
+        exceptionThrown = true;
+    }
+
+    ASSERT_TRUE(exceptionThrown);
+}
+
+TEST_F(XmlReaderTest, exceptionShouldBeThrownWhenFirstNodeDoesNotMatch) {
+    prepareContent();
+
+    bool exceptionThrown = false;
+
+    try {
+        xmlReader_.parse(content_, "ssservverrs");
+    } catch(eMU::core::common::Exception &exception) {
+        exceptionThrown = true;
+    }
+
+    ASSERT_TRUE(exceptionThrown);
+}
+
+TEST_F(XmlReaderTest, parseContentWithoutRecords)
+{
+    content_ = "<servers></servers>";
+    xmlReader_.parse(content_, "servers");
+
+    ASSERT_TRUE(xmlReader_.end());
+
+    xmlReader_.clear();
+}
+
+TEST_F(XmlReaderTest, getShouldReturnDefaultValueWhenContentDoesNotHaveAtributes) {
+    content_ = "<servers><server /></servers>";
+    xmlReader_.parse(content_, "servers");
+
+    ASSERT_FALSE(xmlReader_.end());
+
+    EXPECT_EQ("default", xmlReader_.get<std::string>("server", "name", "default"));
+    EXPECT_EQ("", xmlReader_.get<std::string>("server", "name"));
+
+    EXPECT_EQ(0, xmlReader_.get<int32_t>("server", "code"));
+    EXPECT_EQ(45, xmlReader_.get<int32_t>("server", "code", 45));
+
+    xmlReader_.clear();
+}
