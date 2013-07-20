03a0ddc6abbce89b75d4fa46cf7858b3f8a24ee9
bezloginu666@gmail.com <bezloginu666@gmail.com@5adc7dac-3e50-ac3d-f90e-e331e26be68f>
2013-07-20 18:33:37 +0000

    Added xml reader.

---
+++ src/core/common/xmlReader.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+#include <boost/lexical_cast.hpp>
+#include <rapidxml.hpp>
+
+namespace eMU {
+namespace core {
+namespace common {
+
+class XmlReader {
+public:
+    XmlReader();
+
+    void parse(std::string content, std::string firstNodeName);
+    bool end();
+    void next();
+    void clear();
+
+    template<typename DataType>
+    DataType get(const std::string &nodeName, const std::string &attributeName, const DataType &defaultValue = DataType()) {
+        if(this->end()) {
+            return defaultValue;
+        }
+
+        if(currentNode_->name() != nodeName) {
+            return defaultValue;
+        }
+
+        for (rapidxml::xml_attribute<> *attribute = currentNode_->first_attribute(); attribute != nullptr; attribute = attribute->next_attribute()) {
+            if(attribute->name() == attributeName) {
+                return boost::lexical_cast<DataType>(attribute->value());
+            }
+        }
+
+        return defaultValue;
+    }
+
+private:
+   rapidxml::xml_document<> document_;
+   rapidxml::xml_node<> *currentNode_;
+};
+
+}
+}
+}
