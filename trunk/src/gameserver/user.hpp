326cb544160525dc93e59090cf59d718af2df20e
bezloginu666@gmail.com <bezloginu666@gmail.com@5adc7dac-3e50-ac3d-f90e-e331e26be68f>
2013-11-04 22:21:41 +0000

    Skeleton of gameserver.

---
+++ trunk/src/gameserver/user.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <core/common/hashableObject.hpp>
+
+namespace eMU
+{
+namespace gameserver
+{
+
+class User: public eMU::core::common::HashableObject
+{
+public:
+
+private:
+};
+
+}
+}
