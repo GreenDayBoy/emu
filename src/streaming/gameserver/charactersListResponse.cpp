5f57b4db5782915cda4545708e33f0b15c615e9f
bezloginu666 <bezloginu666@5adc7dac-3e50-ac3d-f90e-e331e26be68f>
2014-01-07 18:11:22 +0000

    Unit tests for characters list response transaction.

--- src/streaming/gameserver/charactersListResponse.cpp
+++ src/streaming/gameserver/charactersListResponse.cpp
@@ -13,6 +13,33 @@ namespace gameserver
 CharactersListResponse::CharactersListResponse(const ReadStream &readStream):
     readStream_(readStream)
 {
+    readStream_.readNext<uint32_t>();
+    readStream_.readNext<uint32_t>();
+    readStream_.readNext<uint32_t>();
+
+    uint16_t charactersCount = readStream_.readNext<uint16_t>();
+
+    for(uint16_t i = 0; i < charactersCount; ++i)
+    {
+        readStream_.readNext<uint32_t>();
+        uint32_t characterNameLength = readStream_.readNext<uint32_t>();
+
+        dataserver::CharacterListInfo characterListInfo;
+        characterListInfo.name_ = boost::locale::conv::utf_to_utf<std::string::value_type>(readStream_.readNextWideString(characterNameLength));
+        characterListInfo.level_ = readStream_.readNext<uint8_t>();
+        readStream_.readNext<uint8_t>();
+        readStream_.readNext<uint8_t>();
+        characterListInfo.race_ = readStream_.readNext<uint8_t>();
+        readStream_.readNext<uint16_t>();
+        characterListInfo.hairType_ = readStream_.readNext<uint8_t>();
+        characterListInfo.hairColor_ = readStream_.readNext<uint8_t>();
+        readStream_.readNext<uint32_t>();
+        readStream_.readNext<uint32_t>();
+        readStream_.readNext<uint16_t>();
+        characterListInfo.tutorialState_ = readStream_.readNext<uint16_t>();
+
+        characters_.push_back(characterListInfo);
+    }
 }
 
 CharactersListResponse::CharactersListResponse(const dataserver::CharacterListInfoContainer &characters):
@@ -33,13 +60,12 @@ CharactersListResponse::CharactersListResponse(const dataserver::CharacterListIn
         writeStream_.writeNext<uint8_t>(0); // dummy
         writeStream_.writeNext<uint8_t>(1); // dummy
         writeStream_.writeNext<uint8_t>(characterInfo.race_);
-        writeStream_.writeNext<uint8_t>(0); // dummy
-        writeStream_.writeNext<uint8_t>(0); // dummy
+        writeStream_.writeNext<uint16_t>(0); // dummy
         writeStream_.writeNext<uint8_t>(characterInfo.hairType_);
         writeStream_.writeNext<uint8_t>(characterInfo.hairColor_);
         writeStream_.writeNext<uint32_t>(0); // dummy
-        writeStream_.writeNext<uint16_t>(0); // dummy
         writeStream_.writeNext<uint32_t>(0); // dummy
+        writeStream_.writeNext<uint16_t>(0); // dummy
         writeStream_.writeNext<uint16_t>(characterInfo.tutorialState_); // tutorial state
     }
 }
@@ -47,6 +73,11 @@ CharactersListResponse::CharactersListResponse(const dataserver::CharacterListIn
 const WriteStream& CharactersListResponse::getWriteStream() const
 {
     return writeStream_;
+}
+
+const dataserver::CharacterListInfoContainer& CharactersListResponse::getCharacters() const
+{
+    return characters_;
 }
 
 }
